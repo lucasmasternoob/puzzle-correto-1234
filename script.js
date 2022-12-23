8ea0f1896cc62c01e8fb5109729ec347acac5e71
LucasAds <105825453+lucasmasternoob@users.noreply.github.com>
2022-12-23 10:19:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+<script>
+var origem = 0;
+var destino = 0;
+var conteudoOrigem = "";
+var conteudoDestino = "";
+
+
+
+function mudar (click) {
+    if(origem == 0){
+        //SE ORIGEM IGUAL A 0 PEGA O VALOR DO PRIMEIRO ELEMENTO CLICADO
+        conteudoOrigem = click.innerHTML
+        origem = click
+    } else{
+        //SENÃO PEGA O VALOR DO SEGUNDO ELEMENTO CLICADO E ARMAZENA PARA A TROCA
+        conteudoDestino = click.innerHTML
+        destino = click
+    }
+    if(origem != 0 && destino != 0){
+        //SE ORIGEM E DESTINO DIFERENTES DE 0, ORIGEM RECEBE O CONTEUDO DESTINO E DESTINO RECEBE CONTEUDO ORIGEM ASSIM EFETUANDO A TROCA DE VALORES
+        //ORIGEM E DESTINO IGUALADOS A 0 PARA QUE RESETE APOS SER FEITA A PRIMEIRA TROCA.
+        origem.innerHTML = conteudoDestino
+        destino.innerHTML = conteudoOrigem
+        origem = 0
+        destino = 0
+    }
+}
+</script>
